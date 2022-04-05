8ee5c768c752825bfa16a1ea9f0b8d4143a3e586
enygf <gg.wp.0004.18@gmail.com>
2022-04-05 13:30:29 +0300

    clear and procent fn's are done

--- src/index.js
+++ src/index.js
@@ -28,10 +28,6 @@ class Calculator {
     }
   }
 
-  getOperation() {
-    return this.props.operation;
-  }
-
   displayValue() {
     document.querySelector(".display-content").innerHTML =
       calc.props.currentValue;
@@ -82,6 +78,26 @@ class Calculator {
     }
   }
 
+  procent() {
+    if (this.props.currentValue && this.props.prevValue) {
+      this.props.currentValue = (
+        (Number(this.props.prevValue) * Number(this.props.currentValue)) /
+        100
+      ).toString();
+      this.props.prevValue = "";
+      this.displayValue();
+    }
+  }
+
+  clear() {
+    this.props.currentValue = "0";
+    this.props.prevValue = "";
+    this.props.operation = null;
+    this.props.memory = "";
+    this.props.history = [];
+    this.displayValue();
+  }
+
   doOperation(operation) {
     switch (operation) {
       case "+":
@@ -95,6 +111,9 @@ class Calculator {
         break;
       case "/":
         this.divide();
+        break;
+      case "%":
+        this.procent();
         break;
       default:
         break;
@@ -122,16 +141,6 @@ class CommandPushOperation {
   }
 }
 
-class CommandGetOperation {
-  constructor(calculator) {
-    this.calculator = calculator;
-  }
-
-  execute() {
-    return this.calculator.getOperation();
-  }
-}
-
 class commandGetValue {
   constructor(calculator) {
     this.calculator = calculator;
@@ -152,46 +161,6 @@ class commandCalculate {
   }
 }
 
-class Plus {
-  constructor(calculator) {
-    this.calculator = calculator;
-  }
-
-  execute() {
-    this.calculator.plus();
-  }
-}
-
-class Minus {
-  constructor(calculator) {
-    this.calculator = calculator;
-  }
-
-  execute() {
-    this.calculator.minus();
-  }
-}
-
-class Divide {
-  constructor(calculator) {
-    this.calculator = calculator;
-  }
-
-  execute() {
-    this.calculator.divide();
-  }
-}
-
-class Multiply {
-  constructor(calculator) {
-    this.calculator = calculator;
-  }
-
-  execute() {
-    this.calculator.multiply();
-  }
-}
-
 const calc = new Calculator();
 const getValue = new commandGetValue(calc);
 const valueExecuter = new Executer(getValue);
@@ -202,21 +171,6 @@ const operationExecuter = new Executer(pushOperation);
 const doCalculation = new commandCalculate(calc);
 const calculationExecuter = new Executer(doCalculation);
 
-const getOperation = new CommandGetOperation(calc);
-const getOperationExecuter = new Executer(getOperation);
-
-// const plus = new Plus(calc);
-// const plusExecuter = new Executer(plus);
-
-// const minus = new Minus(calc);
-// const minusExecuter = new Executer(minus);
-
-// const divide = new Divide(calc);
-// const divideExecuter = new Executer(divide);
-
-// const multiply = new Multiply(calc);
-// const multiplyExecuter = new Executer(multiply);
-
 document.querySelectorAll(".app-controls-button").forEach((item) => {
   item.addEventListener("click", () => {
     if (!isNaN(item.innerHTML) || Number(item.innerHTML)) {
@@ -224,6 +178,9 @@ document.querySelectorAll(".app-controls-button").forEach((item) => {
       console.log(calc);
     } else if (item.innerHTML === "=") {
       calculationExecuter.execute(`${calc.props.operation}`);
+      console.log(calc);
+    } else if (item.innerHTML === "C") {
+      calc.clear();
       console.log(calc);
     } else {
       operationExecuter.execute(item.innerHTML);
